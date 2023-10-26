5c938561f6c0eadedd2b3bd8045a5a54eb7417b6
Eero Kokkonen <eero.kokkonen@hotmail.com>
2023-10-26 13:02:30 +0300

    Tietokannan ja backendin alustus

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+//const userRoutes = require("./routes/users");
+require("dotenv").config();
+const cors = require("cors");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Cors asetukset
+const corsOptions = {
+  origin: process.env.ORIGIN || "http://localhost:5173",
+  optionsSuccessStatus: 200, // For legacy browser support
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+
+//app.use("/api/users", userRoutes);
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+module.exports = app;
