4ba3b156868a735c82ce6a00e6081ae354f1f2da
ijasilari <ijasilari@gmail.com>
2023-11-28 17:22:55 +0200

    Navbarin muutos daisyUIn komponentiksi

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,32 @@
 
+
 const Navbar = () => {
   return (
-    <nav className="bg-gradient-to-r from-indigo-400 to-blue-500 p-4">
-      <div className="container mx-auto flex items-center justify-between">
-        <h1  className="text-2xl font-semibold text-white"> <a href="/">Task Manager</a></h1>
-        <div>
-          <a href="/" className="text-white hover:text-gray-200 px-3 py-2">Tracks</a>
-          <a href="#" className="text-white hover:text-gray-200 px-3 py-2">Login</a>
+    <div className="navbar bg-base-100 shadow-md">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+          </div>
+          <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            <li><a href="/">Tracks</a></li>
+          </ul>
         </div>
+        <a className="flex-0 btn btn-ghost px-2" href="/">
+          <div className="font-title inline-flex text-lg md:text-2xl">
+            OnBoardingTool
+          </div>
+        </a>
       </div>
-    </nav>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li><a>Tracks</a></li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn btn-outline btn-secondary">Login</a>
+      </div>
+  </div>
   );
 };
 
